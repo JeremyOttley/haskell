066ccd707ef4f417f6893342a08281aab085cc36
Jeremy Ottley <Gazaunga@gmail.com>
2025-01-21 09:17:44 -0500

    Update titleCase.hs

--- titleCase.hs
+++ titleCase.hs
@@ -6,6 +6,13 @@ lowerCase = map toLower
 upperCase :: String -> String
 upperCase = map toUpper
 
+capitalize :: [Char] -> String
+capitalize [] = []
+capitalize (x:xs) = toUpper x : map toLower xs
+
+normalize :: String -> String
+normalize xs = map toLower xs
+
 main = do
   let name = "Jeremy"
   putStrLn $ lowerCase name
