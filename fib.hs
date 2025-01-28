02d8b219e107ae6090fa5f325a91a84fe1b4206a
Jeremy Ottley <Gazaunga@gmail.com>
2025-01-28 09:54:56 -0500

    Update fib.hs

--- fib.hs
+++ fib.hs
@@ -7,5 +7,11 @@ fib n
   | n == 1 = 1
   | otherwise = fib (n -1) + fib (n - 2)
 
+-- Alternative form
+-- fib :: Int -> Int
+-- fib 0 = 1
+-- fib 1 = 1
+-- fib n = fib (n - 1) + fib (n - 2)
+
 main = do
   print $ fib 25
