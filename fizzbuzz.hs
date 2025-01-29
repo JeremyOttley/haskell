a1dec000efb175c43d30959ec0a0903387b0994a
Jeremy Ottley <Gazaunga@gmail.com>
2025-01-29 10:36:35 -0500

    Update fizzbuzz.hs

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -15,5 +15,18 @@ fizzbuzz n
   | multOf3 n = "Fizz!"
   | otherwise = show n
 
+-- Alternative using `where` expression
+-- fizzbuzz :: Int -> String
+-- fizzbuzz n
+--   | multOf15 n = "FizzBuzz!"
+--   | multOf5 n = "Buzz!"
+--   | multOf3 n = "Fizz!"
+--   | otherwise = show n
+--   where 
+--     multOf3 n = n `mod` 3 == 0
+--     multOf5 n = n `mod` 5 == 0
+--     multOf15 n = multOf3 n && multOf5 n
+
+
 main = do
   print $ map fizzbuzz [1..20]
